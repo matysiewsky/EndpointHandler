fc5b925246d48a029d58c963a6baf9532e83ac65
Maciej M <matysiewskimaciej@gmail.com>
2022-02-10 17:03:39 +0100

    HttpService implemented with IHttpService interface

--- EndpointHandler.Domain/Interfaces/Services/IHttpService.cs
+++ EndpointHandler.Domain/Interfaces/Services/IHttpService.cs
@@ -1,7 +1,9 @@
+using System.Threading.Tasks;
+
 namespace EndpointHandler.Domain.Interfaces.Services
 {
     public interface IHttpService
     {
-        
+        Task GetResponseAsync();
     }
 }
--- EndpointHandler.Infrastructure/HttpService.cs
+++ EndpointHandler.Infrastructure/HttpService.cs
@@ -1,9 +1,28 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using EndpointHandler.Domain.Interfaces.Services;
 
 namespace EndpointHandler.Infrastructure
 {
-    public class HttpService: IHttpService
+    public class HttpService : IHttpService
     {
+        private readonly HttpClient _httpClient;
+        private readonly string _endpointUri;
 
+        public HttpService(HttpClient httpClient, string endpointUri)
+        {
+            _httpClient = httpClient;
+
+            if (!CheckIfEndpointUriCorrect(endpointUri)) throw
+                new ArgumentException("Endpoint URI can not be empty or null.");
+
+            _endpointUri = endpointUri;
+        }
+        private static bool CheckIfEndpointUriCorrect(string endpointUri)
+            => !string.IsNullOrEmpty(endpointUri);
+
+        public Task GetResponseAsync()
+            => _httpClient.GetAsync(_endpointUri);
     }
 }
