06c262706c8013a592e352dace193ab6cb59ff9a
Maciej M <matysiewskimaciej@gmail.com>
2022-02-10 22:36:49 +0100

    Basic program flow in console version implemented

--- EndpointHandler.ConsoleUI/Program.cs
+++ EndpointHandler.ConsoleUI/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using EndpointHandler.Domain;
+using EndpointHandler.Domain.Interfaces.Services;
+using EndpointHandler.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace EndpointHandler.ConsoleUI
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IConfiguration configuration = ReadConfiguration();
+
+            IHttpService httpService = new HttpService(new HttpClient(), configuration["EndpointToHandle"]);
+            IFileService fileService = new FileService(configuration["FilePath"]);
+
+            try
+            {
+                string response = await httpService.GetResponseAsync();
+                ApiRecord apiRecord = ApiRecordFactory.DeserializeJsonIntoRecord(response);
+
+                fileService.AppendLineToFile(apiRecord.ToString());
+                Console.WriteLine(@$"The following result is saved to file:
+'{apiRecord}'");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine($"The program flow ended. Press any key to close the application.");
+                Console.ReadLine();
+            }
         }
+
+        private static IConfiguration ReadConfiguration()
+            => new ConfigurationBuilder()
+                    .AddJsonFile(path: "appsettings.Development.json", optional: false, reloadOnChange: false)
+                    .Build();
     }
 }
