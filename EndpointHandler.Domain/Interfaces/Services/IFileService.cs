760e3c73f2b58b6b5156a1f2aeb58c1fae8fc343
Maciej M <matysiewskimaciej@gmail.com>
2022-02-10 15:33:09 +0100

    FileService implemented with the IFileService interface

--- EndpointHandler.Domain/Interfaces/Services/IFileService.cs
+++ EndpointHandler.Domain/Interfaces/Services/IFileService.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
+
 namespace EndpointHandler.Domain.Interfaces.Services
 {
     public interface IFileService
     {
-
+        void AppendLineToFile(string lineToWrite);
+        void AppendLinesToFile(IEnumerable<string> linesToWrite);
     }
 }
--- EndpointHandler.Infrastructure/FileService.cs
+++ EndpointHandler.Infrastructure/FileService.cs
@@ -1,9 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using EndpointHandler.Domain.Interfaces.Services;
 
 namespace EndpointHandler.Infrastructure
 {
     public class FileService: IFileService
     {
+        private string FilePath { get; }
+        public FileService(string filePath)
+        {
+            if (!CheckIfPathCorrect(filePath)) throw new ArgumentException("File name can not be empty or null.");
+
+            FilePath = filePath;
+        }
+
+        private static bool CheckIfPathCorrect(string filePath)
+            => !string.IsNullOrEmpty(filePath);
+
+        public void AppendLineToFile(string lineToWrite)
+        {
+            using StreamWriter writer = File.AppendText(FilePath);
+            writer.WriteLine(lineToWrite);
+        }
+
+        public void AppendLinesToFile(IEnumerable<string> linesToWrite)
+            => File.AppendAllLines(FilePath, linesToWrite);
     }
 }
