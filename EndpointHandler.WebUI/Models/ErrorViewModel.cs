cd9ea096a76fb44513be83e859a5e54dd2bfb2ed
Maciej M <matysiewskimaciej@gmail.com>
2022-02-11 15:20:23 +0100

    Deleted

--- EndpointHandler.WebUI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EndpointHandler.WebUI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
